b31d2086793a50ebac8beca78c82e0286de2fa7b
allpandasarecute <89693496+allpandasarecute@users.noreply.github.com>
2025-02-17 22:55:06 +0300

    Smth

---
+++ pr1/inc/color.hpp
@@ -0,0 +1,6 @@
+#include <cstddef>
+#pragma pack(push, 1)
+class Color {
+	std::byte r, g, b;
+};
+#pragma pack(pop)
--- pr1/src/main.cpp
+++ pr1/src/main.cpp
@@ -1,6 +1,8 @@
 #include "test.hpp"
+#include "vec.hpp"
 
 int main() {
 	print(5);
+	auto v = Vec3(1, 2, 3);
 	return 0;
 }
