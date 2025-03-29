527e147bbb2950996f1cbe21fe63f33560e61fd1
allpandasarecute <89693496+allpandasarecute@users.noreply.github.com>
2025-03-29 19:20:09 +0300

    Add odin scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 SCRIPTPATH=$(dirname "$(realpath "$0")")
-BUILDPATH="$SCRIPTPATH/build"
-mkdir -p $BUILDPATH
-cmake -S $SCRIPTPATH -B $BUILDPATH -DCMAKE_BUILD_TYPE=Release 
-cmake --build $BUILDPATH
+# BUILDPATH="$SCRIPTPATH/build"
+# mkdir -p $BUILDPATH
+# cmake -S $SCRIPTPATH -B $BUILDPATH -DCMAKE_BUILD_TYPE=Release 
+# cmake --build $BUILDPATH
+odin build "$SCRIPTPATH/odin" -o:speed
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 SCRIPTPATH=$(dirname "$(realpath "$0")")
-BUILDPATH="$SCRIPTPATH/build"
-$BUILDPATH/pr2 $1 $2
+# BUILDPATH="$SCRIPTPATH/build"
+# $BUILDPATH/pr2 $1 $2
+$SCRIPTPATH/odin.bin $1 $2
